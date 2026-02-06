480358e0d9cddc47672f1d2b7d6ff8fa257fbd32
hankyul <developer.kyul@gmail.com>
2026-02-06 13:06:26 +0900

    학습 : JSON

--- javascript-core/67-AJAX/vite.config.mjs
+++ javascript-core/67-AJAX/vite.config.mjs
@@ -1,23 +1,23 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   server: {
     port: 3000,
-    open: "/learn.html",
+    open: '/learn.html',
   },
   preview: {
     port: 3002,
-    open: "/learn.html",
+    open: '/learn.html',
   },
   build: {
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, "learn.html"),
+        main: path.resolve(__dirname, 'learn.html'),
       },
     },
   },
-});
+})
